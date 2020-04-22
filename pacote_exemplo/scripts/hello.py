bd3c33e62437552f125eb7dac91626912eaf0814
hugo mendes <hugospm@al.insper.edu.br>
2020-04-22 15:35:20 -0300

    adicionando também os arquivos gerados pelo pyinstaller

---
+++ pacote_exemplo/scripts/hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from dev_aberto import hello
+
+if __name__ == '__main__':
+    date, name = hello()
+    print('Último commit feito em:', date, ' por', name)
---
+++ pacote_exemplo/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(name='dev_aberto_hugospm',
+    version='0.1',
+    packages=['dev_aberto'],
+    install_requires = ["requests"],
+    scripts=['scripts/hello.py']
+)
